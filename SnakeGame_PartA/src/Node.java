0f44d04efea2382ae2a74e05093d0cd4eacce79c
kv13 <34185104+kv13@users.noreply.github.com>
2018-12-19 21:20:10 +0200

    Add files via upload

--- SnakeGame_PartA/src/Node.java
+++ SnakeGame_PartA/src/Node.java
@@ -1,275 +1,95 @@
-/*import java.util.ArrayList;
+import java.util.ArrayList;
 public class Node {
 	Node parent;
-	ArrayList<Node> children;
+	ArrayList<Node>children;
 	int nodeDepth;
 	int[] nodeMove;
 	Board nodeBoard;
 	double nodeEvaluation;
 	public Node() {
 		parent=null;
-		children=null;;
-		nodeDepth=0;
+		children=null;
 		nodeMove=null;
 		nodeBoard=null;
 		nodeEvaluation=0;
+		nodeDepth=0;
 	}
-	public Node(int[] nMove,int nDepth,Board nBoard){
-		System.out.println("Inside the constructor for root");
+	public Node(int nDepth,Board nBoard) {
+		//this is the constructor for the roots
+		parent=new Node();//maybe this one is not need it 
 		parent=null;
 		nodeDepth=nDepth;
-		nodeMove=null;
 		nodeBoard=new Board(nBoard);
+		nodeMove=null;
+		children=new ArrayList<Node>(6);
 		nodeEvaluation=0;
-		children=new ArrayList<Node>(6);//because everytime there is 6 possible moves
+	}
+	public Node(Node nParent,int[] nMove,int nDepth,Board nBoard,double Eval) {
+		//this is the constructor for every node that is not a root
+		parent=new Node();//maybe this one is not necessary
+		parent=nParent;
 		nodeMove=new int[nMove.length];
+		for(int i=0;i<nodeMove.length;i++) {
+			nodeMove[i]=nMove[i];
+		}
+		nodeDepth=nDepth;
+		nodeBoard=new Board(nBoard);
+		nodeEvaluation=Eval;
+		children=new ArrayList<Node>(6);
 	}
-	
-	public void set_Parent(Node Parent) {
-		parent=Parent;
+	public Node(Node x) {
+		parent=new Node();//maybe this is not necessary
+		parent=x.get_parent();
+		nodeDepth=x.get_depth();
+		nodeMove=new int[x.get_move().length];
+		for(int i=0;i<nodeMove.length;i++) {
+			nodeMove[i]=x.get_move()[i];
+		}
+		nodeBoard=new Board(x.get_board());
+		nodeEvaluation=x.get_evaluation();
+		children=new ArrayList<Node>();
+		for(int i=0;i<x.get_children().size();i++) {
+			children.add(x.get_children().get(i));
+		}
 	}
-	 public Node get_Parent() {
+	public Node get_parent() {
 		return parent;
 	}
-	 public void set_nodeBoard(Board board) {
-		 nodeBoard=new Board(board);
-	 }
-	 public Board get_nodeBoard() {
-		 return nodeBoard;
-	 }
-	 public void  set_Evaluation(double eval) {
-		 nodeEvaluation=eval;
-	 }
-	 public double get_Evaluation() {
-		 return nodeEvaluation;
-	 }
-	 public void set_Depth(int depth) {
-		 nodeDepth=depth;
-	 }
-	 public int get_Depth() {
-		 return nodeDepth;
-	 }
-	 public void set_Move(int[] move) {
-		 nodeMove=move;
-	 }
-	 public int[] get_Move() {
-		 return nodeMove;
-	 }
-	 public void set_children(Node child) {
-		 children.add(child);
-	 }
-	 public ArrayList<Node> get_children(){
-		 return children;
-	 }
-}*/
-/*import java.util.ArrayList;
-public class Node{
-	Node parent;
-	ArrayList<Node> children;
-	int nodeDepth;
-	int[] nodeMove;
-	Board nodeBoard;
-	double nodeEvaluation;
-	public Node(){
-		parent=null;
-		children=null;
-		nodeMove=null;
-		nodeBoard=null;
-		nodeEvaluation=0;
-		nodeDepth=0;
-	}
-	public Node(int nDepth,Board nBoard){
-		System.out.println("this is the constructor for the root");
-		parent=null;
-		nodeDepth=nDepth;
-		children=new ArrayList<Node>(6);
-		nodeMove=null;
-		nodeEvaluation=0;
-		nodeBoard=new Board(nBoard);		
-	}
-	public Node(Node nParent,int[] nMove,int nDepth,Board nBoard,double nEval){
-		nodeBoard=new Board(nBoard);
-		nodeDepth=nDepth;
-        nodeEvaluation=nEval;
-        children=new ArrayList<Node>(6);
-        nodeMove=new int[nMove.length];
-        for(int i=0;i<nMove.length;i++){
-        	nodeMove[i]=nMove[i];
-        }
-        if(nParent.get_Parent()==null){
-        	parent=new Node(nParent.get_Depth(),nParent.get_nodeBoard());
-        }
-        else if(nParent.get_Parent()!=null){
-        	parent=new Node(nParent.get_Parent(),nParent.get_Move(),nParent.get_Depth(),nParent.get_nodeBoard(),nParent.get_Evaluation());
-        }
-    }
-	public Node(Node X) {
-	}
-    public void set_Parent(Node nParent){
-    	if(nParent.get_Parent()==null){
-    		parent=new Node(nParent.get_Depth(),nParent.get_nodeBoard());
-    	}
-    	else if(nParent.get_Parent()!=null) {
-    		parent=new Node(nParent.get_Parent(),nParent.get_Move(),nParent.get_Depth(),nParent.get_nodeBoard(),nParent.get_Evaluation());
-    	}
-    }
-    public Node get_Parent(){
-    	return parent;
-    }
-	public void set_nodeBoard(Board nBoard){
-		nodeBoard=new Board(nBoard);
-	}
-	public Board get_nodeBoard(){
-		return nodeBoard;
-	}	
-    public void  set_Evaluation(double eval) {
-        nodeEvaluation=eval;
-	}
-	public double get_Evaluation() {
-		return nodeEvaluation;
-	}
-	public void set_Depth(int depth) {
-		nodeDepth=depth;
-	}
-	public int get_Depth() {
-		return nodeDepth;
-	}
-	public void set_Move(int[] move){
-		nodeMove=new int[move.length];
-		for(int i=0;i<move.length;i++){
-			nodeMove[i]=move[i];
-		}
-	}
-	public int[] get_Move(){
-		return nodeMove;
-	}
-	public void set_children(Node child){
-		Node X=new Node(child);
-		children.add(X);
-		//this one has a problem
+	public void set_parent(Node nParent) {
+		parent=nParent;
 	}
 	public ArrayList<Node> get_children(){
 		return children;
 	}
-}
-*/
-//Node class
-//it maybe has problem in the constructors because i did not take care of initialize the ArrayList .
-import java.util.ArrayList;
-public class Node{
-    Node parent;
-	ArrayList<Node> children;
-	int nodeDepth;
-	int[] nodeMove;
-	Board nodeBoard;
-	double nodeEvaluation;
-	public Node(){
-		parent=null;
-		children=null;
-		nodeMove=null;
-		nodeBoard=null;
-		nodeEvaluation=0;
-		nodeDepth=0;
+	public Node get_child(int pointer) {
+		return children.get(pointer);
 	}
-	public Node (Node X){
-		nodeDepth=X.get_Depth();
-		nodeBoard=new Board(X.get_nodeBoard());
-		nodeEvaluation=X.get_Evaluation();
-		nodeMove=new int[X.get_Move().length];
-		for(int i=0;i<X.get_Move().length;i++){
-			nodeMove[i]=X.get_Move()[i];
-		}
-		if(X.get_Parent()==null){
-        	parent=new Node(X.get_Parent().get_Depth(),X.get_Parent().get_nodeBoard());
-        }
-        else if(X.get_Parent()!=null){
-        	parent=new Node(X.get_Parent(),X.get_Parent().get_Move(),X.get_Parent().get_Depth(),X.get_Parent().get_nodeBoard(),X.get_Parent().get_Evaluation());
-        }
-		for(int i=0;i<X.get_children().size();i++) {
-			children.add(X.get_child(i));
-		}
+	public void add_toChildren(Node x) {
+		children.add(x);
 	}
-	public Node(int nDepth,Board nBoard){
-		System.out.println("this is the constructor for the root");
-		parent=null;
-		nodeDepth=nDepth;
-		children=new ArrayList<Node>(6);
-		nodeMove=null;
-		nodeEvaluation=0;
-		nodeBoard=new Board(nBoard);		
-	}
-	public Node(Node nParent,int[] nMove,int nDepth,Board nBoard,double nEval){
-		nodeBoard=new Board(nBoard);
-		nodeDepth=nDepth;
-        nodeEvaluation=nEval;
-        children=new ArrayList<Node>(6);
-        nodeMove=new int[nMove.length];
-        for(int i=0;i<nMove.length;i++){
-        	nodeMove[i]=nMove[i];
-        }
-        /*
-        if(nParent.get_Parent()==null){
-        	parent=new Node(nParent.get_Depth(),nParent.get_nodeBoard());
-        }
-        else if(nParent.get_Parent()!=null){
-        	parent=new Node(nParent.get_Parent(),nParent.get_Move(),nParent.get_Depth(),nParent.get_nodeBoard(),nParent.get_Evaluation());
-        }*/
-        parent.nodeDepth=nParent.get_Depth();
-        parent.nodeBoard=new Board(nParent.get_nodeBoard());
-        parent.nodeEvaluation=nParent.get_Evaluation();
-        parent.nodeMove=new int[nParent.get_Move().length];
-        parent.nodeMove=nParent.get_Move();
-        parent.children=new ArrayList<Node>();
-        parent.children=nParent.get_children();
-        
-    }
-    public void set_Parent(Node nParent){
-    	if(nParent.get_Parent()==null){
-    		parent=new Node(nParent.get_Depth(),nParent.get_nodeBoard());
-    	}
-    	else if(nParent.get_Parent()!=null) {
-    		parent=new Node(nParent.get_Parent(),nParent.get_Move(),nParent.get_Depth(),nParent.get_nodeBoard(),nParent.get_Evaluation());
-    	}
-    }
-    public Node get_Parent(){
-    	return parent;
-    }
-	public void set_nodeBoard(Board nBoard){
-		nodeBoard=new Board(nBoard);
-	}
-	public Board get_nodeBoard(){
-		return nodeBoard;
-	}	
-    public void  set_Evaluation(double eval) {
-        nodeEvaluation=eval;
-	}
-	public double get_Evaluation() {
-		return nodeEvaluation;
-	}
-	public void set_Depth(int depth) {
-		nodeDepth=depth;
-	}
-	public int get_Depth() {
-		return nodeDepth;
-	}
-	public void set_Move(int[] move){
-		nodeMove=new int[move.length];
-		for(int i=0;i<move.length;i++){
-			nodeMove[i]=move[i];
-		}
-	}
-	public int[] get_Move(){
+	public int[] get_move() {
 		return nodeMove;
 	}
-	public void set_children(Node child){
-		Node X=new Node(child);
-		children.add(X);
-		//this one has a problem
+	public void  set_move(int currentPos,int move) {
+		nodeMove[0]=currentPos;
+		nodeMove[1]=move;
 	}
-	public ArrayList<Node> get_children(){
-		return children;
+	public Board get_board() {
+		return nodeBoard;
 	}
-	public Node get_child(int i) {
-		return children.get(i);
+	public void set_board(Board nBoard) {
+		nodeBoard=new Board(nBoard);
+	}
+	public int get_depth() {
+		return nodeDepth;
+	}
+	public void set_depth(int depth) {
+		nodeDepth=depth;
+	}
+	public double get_evaluation() {
+		return nodeEvaluation;
+	}
+	public void set_evaluation(double eval) {
+		nodeEvaluation=eval;
 	}
 }
